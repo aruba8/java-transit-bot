d45027701fa531c30490c9a7fa0fa6864ac3dd3b
Erik Khalimov <biomaks@gmail.com>
2016-09-04 21:23:15 -0500

    added request method

--- src/main/java/com/github/transitbot/api/RoutesService.java
+++ src/main/java/com/github/transitbot/api/RoutesService.java
@@ -4,6 +4,9 @@ import com.github.transitbot.utils.RequestSender;
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.JsonNode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * class to work with routes api.
  */
@@ -12,15 +15,27 @@ public class RoutesService {
     /**
      * path.
      */
-    private String routesPath = "routes/";
+    private String routesPath = "routes";
 
     /**
-     * get json by stopNumber.
+     * get json by routeNumber.
      *
+     * @param routeNumber routeNumber
+     * @return json
+     */
+    public HttpResponse<JsonNode> getInfoByRouteNumber(String routeNumber) {
+        return RequestSender.sendRequest(routesPath + "/" + routeNumber, null);
+    }
+
+    /**
+     * get routes by stop number.
      * @param stopNumber stopNumber
      * @return json
      */
     public HttpResponse<JsonNode> getRoutesByStopNumber(String stopNumber) {
-        return RequestSender.sendRequest(routesPath + stopNumber);
+        Map<String, String> params = new HashMap<>();
+        params.put("stop", stopNumber);
+        return RequestSender.sendRequest(routesPath, params);
     }
+
 }
--- src/main/java/com/github/transitbot/utils/RequestSender.java
+++ src/main/java/com/github/transitbot/utils/RequestSender.java
@@ -6,6 +6,8 @@ import com.mashape.unirest.http.JsonNode;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
 
+import java.util.Map;
+
 /**
  * request sender.
  */
@@ -15,16 +17,18 @@ public class RequestSender {
      * static method to send request.
      *
      * @param path represents path of url
+     * @param params url parameters Map<String, Object>
      * @return HttpResponse object. Json object can be retrieved from it
      */
-    public static HttpResponse<JsonNode> sendRequest(String path) {
+    public static HttpResponse<JsonNode> sendRequest(String path, Map params) {
         String apiUrl = ConfigReader.getPropValues().getProperty("transit.url");
         String apiKey = ConfigReader.getPropValues().getProperty("transit.key");
         String jsonExtension = ".json";
         HttpResponse<JsonNode> response = null;
 
         try {
-            response = Unirest.get(apiUrl + path + jsonExtension).queryString("api-key", apiKey).asJson();
+            response = Unirest.get(apiUrl + path + jsonExtension).queryString(params)
+                    .queryString("api-key", apiKey).asJson();
         } catch (UnirestException e) {
             e.printStackTrace();
         }
