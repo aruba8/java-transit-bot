9dac198d1768fa7de7bddcef70343b693c68f543
Erik Khalimov <biomaks@gmail.com>
2017-04-09 16:30:34 -0500

    Location getAddressesWithNameFromLocation

--- src/main/java/com/github/transitbot/api/services/LocationsService.java
+++ src/main/java/com/github/transitbot/api/services/LocationsService.java
@@ -3,9 +3,14 @@ package com.github.transitbot.api.services;
 import com.github.transitbot.api.models.Coordinates;
 import com.github.transitbot.utils.RequestSender;
 import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * LocationsService class.
@@ -28,6 +33,26 @@ public class LocationsService {
         params.put("lat", coordinates.getLatitude());
         params.put("lon", coordinates.getLongitude());
         return RequestSender.sendRequest(routesPath, params);
+    }
+
+    /**
+     * Get locations which have names.
+     *
+     * @param locationResponse locationResponse
+     * @return set of locations
+     */
+    public Set<JSONObject> getAddressesWithNameFromLocation(HttpResponse<JsonNode> locationResponse) {
+        JSONArray rawObject = locationResponse.getBody().getArray();
+        JSONObject locObject = rawObject.getJSONObject(0);
+        JSONArray locationsArray = locObject.getJSONArray("locations");
+        Set<JSONObject> locationsWithName = new HashSet<>();
+        for (Object object : locationsArray) {
+            JSONObject locationObject = (JSONObject) object;
+            if (locationObject.has("name")) {
+                locationsWithName.add(locationObject);
+            }
+        }
+        return locationsWithName;
     }
 
 }
